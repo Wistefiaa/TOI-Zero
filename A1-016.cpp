85e7bea911245808053a9bd8ac416b5154e10472
Nipitphon Cheawchatchawan <yukanzakino@gmail.com>
2025-04-03 18:51:20 +0700

    Update A1-016

---
+++ A1-016.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    string s;
+    cin>>s;
+    if(s[2]=='1'&&s[3]=='6'){
+        cout<<"yes";
+    }
+    else cout<<"no";
+}
